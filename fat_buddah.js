a95d35df8a05de9f21558a2aeda7af66e90c760b
Mika <mikadewitz@gmail.com>
2015-01-17 13:21:36 -1000

    fat buddah js

---
+++ fat_buddah.js
@@ -0,0 +1,49 @@
+<html>
+  <head>
+    <script>   
+
+function buddahSays {
+  function searchTerm {
+    var user = [user input];
+    var adjs = [messy, ugly, super, stupid]; 
+    //Randomize source: http://stackoverflow.com/questions/4550505/getting-random-value-from-an-array
+    var randA = adjs[Math.floor(Math.random() * adjs.length)];
+    var search = user.unshift(randA + " ");
+    }
+    var question = ["What would you like to eat?", "What is your spirit animal?", "what "];
+   //Switch source: https://www.youtube.com/watch?v=Z6O_XdfCBEo 
+   switch (question) {
+    case "what would you like to eat?": 
+      document.write("enjoy your " + search + "motherfucker");
+      break;
+    case "animal":
+      document.write("You're a " + search + "______");
+      break;
+    case "feeling":
+      document.write("This is what " + search + "looks like");
+      break;
+    }
+
+};
+
+
+ /**   
+ //var user = [user input]; 
+  var question = [
+  {'what would you like to eat?': 'enjoy your' + search + },
+  {'second question: second answer' + }];
+
+ //randomizes output
+ var randQ = questions[Math.floor(Math.random() * question.length)];
+
+ return ("enjoy your " + search + " motherfucker");
+
+
+};
+
+if user inputs an answer with unrecognized symbol, return "wat"?  <---------
+*/
+    </script>
+  </head>
+</html>
+
