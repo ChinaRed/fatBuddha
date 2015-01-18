48242bfc8e8ab7f315a88c896bff32cca86d4bbb
Mika <mikadewitz@gmail.com>
2015-01-17 16:30:31 -1000

    js changes

--- fat_buddah.js
+++ fat_buddah.js
@@ -1,30 +1,40 @@
-<html>
-  <head>
-    <script>   
-
-function buddahSays {
-  function searchTerm {
-    var user = [user input];
+   function validateUserInput() {
+    var userResponse = document.forms["myForm"]["userinput"].value;
+    if (x===null || x==="") {
+        alert("try again homie!");
+        return false;
+    }
+}
+function compareUserInput() {
+  var userResponse2 = document.forms["myForm"]["userinput"].value;
+  var userArr = userResponse2.split("");
+  var compare = 
+}
+function searchTerm() {
+    var user = document.getElementById("user").innerHTML;
     var adjs = [messy, ugly, super, stupid]; 
     //Randomize source: http://stackoverflow.com/questions/4550505/getting-random-value-from-an-array
     var randA = adjs[Math.floor(Math.random() * adjs.length)];
     var search = user.unshift(randA + " ");
     }
-    var question = ["What would you like to eat?", "What is your spirit animal?", "what "];
+function buddahSays() {
+    var question = document.getElementById("question").innerHTML;
    //Switch source: https://www.youtube.com/watch?v=Z6O_XdfCBEo 
-   switch (question) {
-    case "what would you like to eat?": 
-      document.write("enjoy your " + search + "motherfucker");
+   switch (userSelction) {
+    case "what food does your heart desire?": 
+      document.write("enjoy your " + search + "mother");
       break;
-    case "animal":
+    case "what is your favorite animal?":
       document.write("You're a " + search + "______");
       break;
     case "feeling":
       document.write("This is what " + search + "looks like");
       break;
     }
+}
 
-};
+
+
 
 
  /**   
@@ -43,7 +53,4 @@ function buddahSays {
 
 if user inputs an answer with unrecognized symbol, return "wat"?  <---------
 */
-    </script>
-  </head>
-</html>
-
+    
