53b9bc0a4cd6b55c32bb62526cf78e5d95bd35c7
Jon <jonathan.tan83@gmail.com>
2015-01-23 14:32:16 -1000

    added Sarah's jquery scripts to index.html and google api code to javascript.js file

--- javascript.js
+++ javascript.js
@@ -1,26 +1,25 @@
 function validateUserInput() {
-    var userResponse = document.forms["myForm"]["userinput"].value;
+    var userResponse = document.forms["myForm"]["yourresponse"].value;
     if (x===null || x==="") {
         alert("try again homie!");
         return false;
     }
 }
 
-/*
 function compareUserInput() {
-  var userResponse2 = document.forms["myForm"]["userinput"].value;
+  var userResponse2 = document.forms["myForm"]["yourresponse"].value;
   var userArr = userResponse2.split("");
-  var compare = 
+  //var compare = ;
 }
 function searchTerm() {
     var user = document.getElementById("user").innerHTML;
-    var adjs = [messy, ugly, super, stupid]; 
+    var adjs = ["messy", "ugly", "super", "stupid"]; 
     //Randomize source: http://stackoverflow.com/questions/4550505/getting-random-value-from-an-array
     var randA = adjs[Math.floor(Math.random() * adjs.length)];
     var search = user.unshift(randA + " ");
     }
 function buddahSays() {
-    var question = document.getElementById("question").innerHTML;
+    var question = document.getElementById("dropdownMenu1").innerHTML;
   //Switch source: https://www.youtube.com/watch?v=Z6O_XdfCBEo 
   switch (userSelction) {
     case "what food does your heart desire?": 
@@ -34,4 +33,26 @@ function buddahSays() {
       break;
     }
 }
+
+
+
+/* 
+SARAH'S CODE
+
+$(document).ready(function(){
+  var search_term = "steve+jobs";
+
+  $.ajax({
+    url: "https://www.googleapis.com/customsearch/v1",
+    data: {
+      key: "AIzaSyBLiL9m64v2o5eSfIcsTQ_Epl_qy8_ZGI8",
+      cx: "004953656570767173379:eugpp6v3_yq",
+      searchType: "image",
+      q: search_term
+    }
+  })
+  .done(function(data) {
+    console.log( "Sample of data:", data );
+  });
+
 */
